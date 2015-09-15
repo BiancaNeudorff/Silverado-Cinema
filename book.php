3ef95de8275be6b479458ecda1d824e7ad403943
Matthew HAYWARD <matt@matthayward.com.au>
2015-09-15 18:20:27 +1000

    Working on booking

--- book.php
+++ book.php
@@ -27,23 +27,23 @@
   $days = [];
   if ($moviecat == "AC"){
     $days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    $times = {"Wednesday":"9pm","Thursday":"9pm","Friday":"9pm","Saturday":"9pm","Sunday":"9pm"};
+    Print "<script>var times = {'Wednesday':'9pm','Thursday':'9pm','Friday':'9pm','Saturday':'9pm','Sunday':'9pm'};</script>";
   }
 
   if ($moviecat == "AF"){
     $days = ["Monday", "Tuesday", "Saturday", "Sunday"];
-    $times = {"Monday":"6pm","Tuesday":"6pm","Saturday":"3pm","Sunday":"3pm"};
+    //$times = {"Monday":"6pm","Tuesday":"6pm","Saturday":"3pm","Sunday":"3pm"};
   }
 
   if ($moviecat == "CH"){
     $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    $times = {"Monday":"9pm","Tuesday":"9pm","Wednesday":"1pm","Thursday":"1pm","Friday":"1pm","Saturday":"6pm","Sunday":"6pm"};
+    //$times = {"Monday":"9pm","Tuesday":"9pm","Wednesday":"1pm","Thursday":"1pm","Friday":"1pm","Saturday":"6pm","Sunday":"6pm"};
   }
 
 
   if ($moviecat == "CH"){
     $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    $times = {"Monday":"1pm","Tuesday":"1pm","Wednesday":"6pm","Thursday":"6pm","Friday":"6pm","Saturday":"12pm","Sunday":"12pm"};
+    //$times = {"Monday":"1pm","Tuesday":"1pm","Wednesday":"6pm","Thursday":"6pm","Friday":"6pm","Saturday":"12pm","Sunday":"12pm"};
   }
 
 ?>
