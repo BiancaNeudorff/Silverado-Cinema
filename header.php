27d42ea573f395cfc72bec433148ea08b08290c7
Matthew HAYWARD <matt@matthayward.com.au>
2015-09-15 17:15:38 +1000

    Moving to php

--- header.php
+++ header.php
@@ -21,9 +21,9 @@
           </div>
           <div class='col col-1'></div>
           <div id='menu' class='col col-6'>
-              <a href='#' onclick='loadPage("home")'>HOME</a>
-              <a href='#' onclick='loadPage("cinema")'>CINEMA</a>
-              <a href='#' onclick='loadPage("movies")'>MOVIES</a>
+              <a href='index.php'>HOME</a>
+              <a href='cinema.php'>CINEMA</a>
+              <a href='movies.php'>MOVIES</a>
               <a href='#' onclick='loadPage("contact")'>CONTACT</a>
           </div>
           <div class='col col-1'></div>
---
+++ movies.php
@@ -0,0 +1,204 @@
+<?php
+  require("header.php");
+?>
+<h1>Movies - Now Showing</h1>
+<div class='row centre'>
+  <div class='movie current-movie'></div>
+</div>
+<div class='row centre'>
+  <div class='col col-3 movie' style='background-image:url("http://2.bp.blogspot.com/-id0szKjvbxA/VV59veSLPhI/AAAAAAAAMlw/c0r2lA_iD2E/s1600/InsideOut54f771108303d.jpg")'>
+    <div class='movie-info-holder'>
+      <div class='movie-info'>
+        <div class='close-btn'><a href='#'>x</a></div>
+        <h1>Inside Out</h1>
+        <!-- Star icon by Icons8 -->
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <!-- Original content below sourced for educational purposes: https://en.wikipedia.org/wiki/Inside_Out_(2015_film) -->
+        <p>
+          Inside Out is a 2015 American 3D computer-animated comedy-drama adventure film produced by Pixar Animation Studios and released by Walt Disney Pictures.
+          Directed and co-written by Pete Docter, the film is set in the mind of a young girl, Riley Andersen (Kaitlyn Dias), where five personified emotions—Joy
+           (Amy Poehler), Sadness (Phyllis Smith), Fear (Bill Hader), Anger (Lewis Black), and Disgust (Mindy Kaling)—try to lead her through life as she moves with
+           her parents (Diane Lane and Kyle MacLachlan) to a new city. The film was co-directed and co-written by Ronnie del Carmen and produced by Jonas Rivera, with
+            music composed by Michael Giacchino.
+        </p>
+        <div class='row'>
+          <div class='col col-4'>
+            <b>Rating - PG</b>
+            <br>
+            <b>Running time - 94 minutes</b>
+            <br><br>
+            <b>Animation, Adventure, Comedy</b>
+          </div>
+          <div class='col col-4'>
+            <b>Showing times</b>
+            <ul>
+              <li>Monday - Tuesday 1PM</li>
+              <li>Wednesday - Friday 6PM</li>
+              <li>Saturday - Sunday 12PM</li>
+            </ul>
+          </div>
+          <div class='col col-4' style='text-align:right'>
+            <a href='book.php?movie=insideout' class='bookbtn'>Book now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='col col-3 movie' style='background-image:url("http://ia.media-imdb.com/images/M/MV5BMTU2MTkwMzM0NF5BMl5BanBnXkFtZTgwMjA0NDA4NTE@._V1_SX640_SY720_.jpg")'>
+    <div class='movie-info-holder'>
+      <div class='movie-info'>
+        <div class='close-btn'><a href='#'>x</a></div>
+        <h1>A Walk in the Woods</h1>
+        <!-- Star icon by Icons8 -->
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <!-- Original content below sourced for educational purposes: https://en.wikipedia.org/wiki/A_Walk_in_the_Woods_(film) -->
+        <p>
+          A Walk in the Woods is a 2015 American adventure comedy biopic film directed by Ken Kwapis, based on the 1998 book/memoir of the same name by Bill Bryson. The film stars Robert Redford, Nick Nolte, Kristen Schaal, Nick Offerman, Mary Steenburgen and Emma Thompson. The film was released on September 2, 2015, by Broad Green Pictures.
+        </p>
+        <div class='row'>
+          <div class='col col-4'>
+            <b>Rating - M</b>
+            <br>
+            <b>Running time - 104 minutes</b>
+            <br><br>
+            <b>Adventure, Comedy, Drama</b>
+          </div>
+          <div class='col col-4'>
+            <b>Showing times</b>
+            <ul>
+              <li>Monday - Tuesday 6PM</li>
+              <li>Saturday - Sunday 3PM</li>
+            </ul>
+          </div>
+          <div class='col col-4' style='text-align:right'>
+            <a href='book.php?movie=awalkinthewoods' class='bookbtn'>Book now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='col col-3 movie' style='background-image:url("http://ia.media-imdb.com/images/M/MV5BMTg4Mjc0NTE1NV5BMl5BanBnXkFtZTgwNzcwNTQ3NTE@._V1_SX640_SY720_.jpg")'>
+    <div class='movie-info-holder'>
+      <div class='movie-info'>
+        <div class='close-btn'><a href='#'>x</a></div>
+        <h1>Aloha</h1>
+        <!-- Star icon by Icons8 -->
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <!-- Original content below sourced for educational purposes: https://en.wikipedia.org/wiki/Aloha_(film) -->
+        <p>
+          While on assignment in Oahu, Hawaii, military contractor Brian Gilcrest (Bradley Cooper) reconnects with his old flame Tracy Woodside (Rachel McAdams), now married to an Air Force recruit (John Krasinski). He also spends time with Allison Ng (Emma Stone), a hard-nosed fighter pilot who watches every move that he makes. As they travel throughout the lush terrain, Brian finds himself falling for his feisty guide, while his conversations with Tracy may provide a shocking revelation from their past.
+        </p>
+        <div class='row'>
+          <div class='col col-4'>
+            <b>Rating - PG</b>
+            <br>
+            <b>Running time - 105 minutes</b>
+            <br><br>
+            <b>Romantic Comedy</b>
+          </div>
+          <div class='col col-4'>
+            <b>Showing times</b>
+            <ul>
+              <li>Monday - Tuesday 9PM</li>
+              <li>Wednesday - Friday 1PM</li>
+              <li>Saturday - Sunday 6PM</li>
+            </ul>
+          </div>
+          <div class='col col-4' style='text-align:right'>
+            <a href='book.php?movie=aloha' class='bookbtn'>Book now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='col col-3 movie' style='background-image:url("http://ia.media-imdb.com/images/M/MV5BMjE0MDI3NTE5NF5BMl5BanBnXkFtZTgwNzI3ODM2NjE@._V1_SX640_SY720_.jpg")'>
+    <div class='movie-info-holder'>
+      <div class='movie-info'>
+        <div class='close-btn'><a href='#'>x</a></div>
+        <h1>No Escape</h1>
+        <!-- Star icon by Icons8 -->
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <img src="images/star-50.png" title="Star" width="24">
+        <!-- Original content below sourced for educational purposes: https://en.wikipedia.org/wiki/Inside_Out_(2015_film) -->
+        <p>
+          American businessman Jack Dwyer (Owen Wilson), wife Annie and their two young daughters arrive in Southeast Asia to begin a new life. As his company plans to improve the region's water quality, the family quickly learns that they're right in the middle of a political uprising. Armed rebels attack the hotel where they're staying, ordered to kill any foreigners that they encounter. Amid utter chaos, Jack must find a way to save himself and his loved ones from the violence erupting all around them.
+        </p>
+        <div class='row'>
+          <div class='col col-4'>
+            <b>Rating - PG</b>
+            <br>
+            <b>Running time - 103 minutes</b>
+            <br><br>
+            <b>Action</b>
+          </div>
+          <div class='col col-4'>
+            <b>Showing times</b>
+            <ul>
+              <br>
+              <li>Wednesday - Friday 9PM</li>
+              <li>Saturday - Sunday 9PM</li>
+            </ul>
+          </div>
+          <div class='col col-4' style='text-align:right'>
+            <a class='bookbtn' href='book.php?movie=noescape'>Book now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<h3>Our schedule</h3>
+<br>
+<b>Monday - Tuesday</b>
+<br><br>
+<div class='schedule-bar schedule-childrens'>Childrens (1PM)</div>
+<div class='schedule-bar schedule-art'>Art / Foreign (6PM)</div>
+<div class='schedule-bar schedule-romance'>Romantic Comedy (9PM)</div>
+<br><br>
+<b>Wednesday - Friday</b>
+<br><br>
+<div class='schedule-bar schedule-romance'>Romantic Comedy (1PM)</div>
+<div class='schedule-bar schedule-childrens'>Childrens (6PM)</div>
+<div class='schedule-bar schedule-action'>Action (9PM)</div>
+<br><br>
+<b>Saturday - Sunday</b>
+<br><br>
+<div class='schedule-bar schedule-childrens'>Childrens (12PM)</div>
+<div class='schedule-bar schedule-art'>Art / Foreign (3PM)</div>
+<div class='schedule-bar schedule-romance'>Romantic Comedy (6PM)</div>
+<div class='schedule-bar schedule-action'>Action (9PM)</div>
+<script>
+//Only run animation if non mobile
+if ($(document).width() > 768){
+  $(".current-movie").hide();
+  $(".movie-info-holder").hide();
+  $(".movie").on("click", function(){
+    $(".movie").css("width","20%");
+    $(".movie").show();
+    $(".movie-info-holder").hide();
+    $(this).hide();
+    $(".current-movie").html($(this).html());
+    $(".current-movie").css("background-image", $(this).css("background-image"));
+    $(".current-movie").css("width","100%");
+    $(".current-movie").find(".movie-info-holder").fadeIn("slow");
+  });
+
+  $(".btn-close").on("click", function(){
+    $(".current-movie").hide();
+    $(".movie").show();
+  });
+}
+</script>
+<?php
+  require("footer.php");
+?>
