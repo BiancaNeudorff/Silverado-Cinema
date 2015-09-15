3a998688b2f703732d355bc297e6403125d22858
Matthew HAYWARD <matt@matthayward.com.au>
2015-09-15 17:09:59 +1000

    Added header and footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+</div>
+<div id='footer' class='row'>
+<hr>
+<div class='col col-1'></div>
+<div class='col col-5'>Silverado Cinema | 10 Blockbuster Road</div>
+<div class='col col-6' style='text-align:right'>Matt Hayward | S3537899</div>
+</div>
+</div>
+</body>
+</html>
